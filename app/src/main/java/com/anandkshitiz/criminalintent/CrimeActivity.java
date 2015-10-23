113fa6a53aa4eef9d56d6e2aa593cf859cb2febe
Kshitiz Anand <k.anand26@gmail.com>
2015-10-23 20:23:56 +0530

    Added fragment transaction

--- app/src/main/java/com/anandkshitiz/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/anandkshitiz/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.anandkshitiz.criminalintent;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,6 +15,16 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer, fragment)
+                .commit();
+        }
     }
 
 
